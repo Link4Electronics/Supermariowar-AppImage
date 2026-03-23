a9a4398379ab1303578d3a778f12dd0dc296a502
Link4Electronics <link4electronics@gmail.com>
2026-03-22 22:31:35 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -13,8 +13,8 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-PRE_BUILD_CMDS="sed -i 's|-DSMW_DATADIR:path=\"/var/lib/smw\"|-DSMW_DATADIR:path=\"\$APPDIR/bin\"|' ./PKGBUILD" make-aur-package supermariowar-git
+PRE_BUILD_CMDS="sed -i 's|-DSMW_DATADIR:path=\"/var/lib/smw\"|-DSMW_DATADIR:path=\"$APPDIR/bin/data\"|' ./PKGBUILD" make-aur-package supermariowar-git
 
 # If the application needs to be manually built that has to be done down here
 mkdir -p ./AppDir/bin
-mv -v /var/lib/smw/* ./AppDir/bin
+#mv -v /var/lib/smw/* ./AppDir/bin
